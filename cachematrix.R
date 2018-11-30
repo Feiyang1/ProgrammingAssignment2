ffac9669104690208f56cf46ff938929ca3c349f
Feiyang1 <plane1113@gmail.com>
2018-11-30 11:36:45 -0800

    delete unused

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-## test test
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
